c50779df55b7b10773bb77c60cd3aaa9b3b6d424
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-07-24 10:15:01 -0500

    Create AppDbContext.cs in SenderApi

---
+++ SenderApi/Repositories/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SenderApi.Repositories
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
