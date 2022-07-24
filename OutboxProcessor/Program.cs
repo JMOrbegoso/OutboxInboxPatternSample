739401dff91a5bbe08054cb332173f0d7ab97385
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-07-23 20:31:36 -0500

    Create OutboxProcessor project

---
+++ OutboxProcessor/Program.cs
@@ -0,0 +1,17 @@
+namespace OutboxProcessor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
