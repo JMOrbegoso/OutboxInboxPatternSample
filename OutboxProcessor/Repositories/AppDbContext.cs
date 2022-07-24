c454ef392f6f5908244e15b58d0fb4cf015818a3
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-07-24 11:07:00 -0500

    Create AppDbContext.cs in OutboxProcessor

---
+++ OutboxProcessor/Repositories/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OutboxProcessor.Repositories
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
