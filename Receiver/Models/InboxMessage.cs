233ee04a6003ae6e27617b96d998f13b7edd91ee
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-07-28 19:07:51 -0500

    Create InboxMessage in Receiver

---
+++ Receiver/Models/InboxMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace Receiver.Models
+{
+    public sealed record InboxMessage(
+        string Id,
+        DateTime CreatedOn,
+        string @Type,
+        string Data)
+    {
+        public DateTime? HandledOn { get; set; } = default!;
+    }
+}
