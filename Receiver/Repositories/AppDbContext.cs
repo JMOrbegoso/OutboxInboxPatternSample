a69e2c6b9ff4f9ab4f107f220e8e4ab482b57b10
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-07-28 20:42:26 -0500

    Update AppDbContext adding InboxMessage in Receiver

--- Receiver/Repositories/AppDbContext.cs
+++ Receiver/Repositories/AppDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Receiver.Models;
 
 namespace Receiver.Repositories
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<InboxMessage> InboxMessages { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
