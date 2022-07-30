84beb8e62524584f5f7c62bca7dd399da6ee1770
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-07-29 20:33:04 -0500

    Update AppDbContext adding User in SenderApi

--- SenderApi/Repositories/AppDbContext.cs
+++ SenderApi/Repositories/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace SenderApi.Repositories
     public class AppDbContext : DbContext
     {
         public DbSet<OutboxMessage> OutboxMessages { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
