9961893a5a430093e20f40128128be160f2c0a6f
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-07-29 20:34:03 -0500

    Add database migration of User schema

--- SenderApi/Migrations/20220730013334_User.Designer.cs
+++ SenderApi/Migrations/20220730013334_User.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SenderApi.Repositories;
 
@@ -11,9 +12,10 @@ using SenderApi.Repositories;
 namespace SenderApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220730013334_User")]
+    partial class User
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
